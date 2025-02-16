8cca9acdcbc3cd85cd5d39724a1acb20cc86cbfa
Edward Lee <jl4519@hurricane.ae.ic.ac.uk>
2025-02-16 17:45:55 +0000

    Tried CBLAS Wrapper in 4.1.

--- 4_1/main.cpp
+++ 4_1/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <iomanip>
+#include <cblas.h>
 #define F77NAME(x) x##_
 
 extern "C"
 {
     double F77NAME(dnrm2)(const int& n, const double* x, const int& incx);
     double F77NAME(dasum)(const int& n, const double* x, const int& incx);
-    double F77NAME(idamax)(const int& n, const double* x, const int& incx);
+    int F77NAME(idamax)(const int& n, const double* x, const int& incx);
 }
 
 int main()
@@ -18,15 +20,20 @@ int main()
     
     for(int i = 0; i < N; ++i)
     {
-        x[i] = (double(rand())) / RAND_MAX;
+        x[i] = (double(rand())) / double(RAND_MAX/10);
     }
     
 
     
     // Compute the Euclidian Norm, the Sum of the abs, and the maximum abs
     double norm2 = F77NAME(dnrm2)(N,x,1);
+    double norm2_c = cblas_dnrm2(N,x,1);
+
     double abssum = F77NAME(dasum)(N,x,1);
-    double maxabs = F77NAME(idamax)(N,x,1);
+    double abssum_c = cblas_dasum(N,x,1);
+
+    int maxabs = F77NAME(idamax)(N,x,1);
+    int maxabs_c = cblas_idamax(N,x,1);
 
     // Print out
     for(int j = 0; j < N; ++j)
@@ -34,9 +41,9 @@ int main()
         std::cout << x[j] << std::endl;
     }
     std::cout << " " << std::endl;
-    std::cout << "Euclidian Norm: " << norm2 << std::endl;
-    std::cout << "Sum of the abs: " << abssum << std::endl;
-    std::cout << "Maximum abs " << maxabs << std::endl;
+    std::cout << "Euclidian Norm: " << norm2 << std::setw(40) << "Cblas: " << norm2_c << std::endl;
+    std::cout << "Sum of the abs: " << abssum << std::setw(40) << "Cblas: " << abssum_c << std::endl;
+    std::cout << "Maximum abs index: " << maxabs << std::setw(40) << "Cblas: " << maxabs_c << std::endl; //Fortran Array indexing uses one-based indexing
 
     delete[] x; //Deallocate the Memory
 
